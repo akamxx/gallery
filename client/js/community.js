8e6b4cbf86003180b15ef1e0f5fc8157fa95facf
nemenyan <180469810+nemenyan@users.noreply.github.com>
2026-05-23 18:01:07 -0400

    community pas fini

--- client/js/community.js
+++ client/js/community.js
@@ -1,11 +1,15 @@
 async function loadGallery() {
     const response = await fetch('/api/gallery');
-};
 
-const drawings = await response.json();
-const gallery = document.getElementById('gallery');
-drawings.forEach(drawing => {
-    const img = document.createElement('img');
-    img.src = drawing.imageUrl;
-    gallery.appendChild(img);
-});
+
+    const drawings = await response.json();
+
+    const gallery = document.getElementById('galerie');
+    drawings.forEach(drawing => {
+        const img = document.createElement('img');
+        img.src = drawing.image;
+        gallery.appendChild(img);
+    });
+}
+
+loadGallery();
