483f43f72cf99e97feae9b9d7de1de0923bb35d7
Camille Marquis <camimarquis@gmail.com>
2026-05-24 10:23:32 -0400

    ajouter des oeuvres

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,24 +1,32 @@
 export const API = {
-  ARTIC_BASE:      "https://api.artic.edu/api/v1",
-  ARTIC_IMG:       "https://www.artic.edu/iiif/2",
-  LOCAL_BASE:      "http://localhost:5000/api",
-  LOCAL_FRONTEND:  "http://localhost:5173",
+  ARTIC_BASE: "https://api.artic.edu/api/v1",
+  ARTIC_IMG: "https://www.artic.edu/iiif/2",
+  LOCAL_BASE: "http://localhost:5000/api",
+  LOCAL_FRONTEND: "http://localhost:5173",
 };
 
-export const ARTIC_IMG_PARAMS       = "full/400,/0/default.jpg";
-export const ARTIC_IMG_PARAMS_MAIN  = "full/843,/0/default.jpg";
-export const ARTIC_ART_FIELDS       = "id,title,image_id";
+export const ARTIC_IMG_PARAMS = "full/400,/0/default.jpg";
+export const ARTIC_IMG_PARAMS_MAIN = "full/843,/0/default.jpg";
+export const ARTIC_ART_FIELDS = "id,title,image_id";
 
-export const DEFAULT_AUTHOR         = "Anonyme";
-export const UNKNOWN_ARTIST         = "Unknown artist";
-export const DATE_LOCALE            = "fr-CA";
-export const DATE_OPTIONS           = { day: "numeric", month: "short", year: "numeric" };
+export const DEFAULT_AUTHOR = "Anonyme";
+export const UNKNOWN_ARTIST = "Unknown artist";
+export const DATE_LOCALE = "fr-CA";
+export const DATE_OPTIONS = { day: "numeric", month: "short", year: "numeric" };
 
-export const ARTWORK_IDS            = [27992, 129884, 111628, 28560, 81539, 6565, 12345, 12101];
-export const PALETTE_COLOR_COUNT    = 8;
+export const ARTWORK_IDS = [
+  27992, 129884, 111628, 28560, 81539, 6565, 12345, 12101, 51719, 16568, 27993,
+  64818, 14647, 20684, 15597, 11723, 229393, 86196, 111442, 111441, 27994,
+  14598, 14600, 28345, 27995, 111627, 20713, 20714, 137125, 111624, 129885,
+  15468, 15469, 86385, 81538, 111625, 111626, 11724, 11725, 11726, 11727, 11728,
+  11729, 11730, 11731, 11732, 11733, 59426, 93798, 14649, 14651, 16571, 18378,
+  18379, 18380, 18381, 18382, 28344, 28346, 28347, 28348, 28349, 28350, 28351,
+  28352, 28353, 28354,
+];
+export const PALETTE_COLOR_COUNT = 8;
 export const CARD_ANIMATION_STEP_MS = 60;
 
 export const ROUTES = {
-  PAINT:     "/paint",
+  PAINT: "/paint",
   COMMUNITY: "/community",
 };
